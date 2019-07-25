b21a4fde9102d2ac9ac811e29d211898165610ba
Anand MV <anandm@qburst.com>
2019-07-26 01:20:22 +0530

    Update firebase-config.js

--- client/src/firebase-config.js
+++ client/src/firebase-config.js
@@ -1,11 +1,11 @@
 var config = {
-    apiKey: "AIzaSyAr63kLwJ1R36cLW5RuNVdXZDXUa_8FatE",
-    authDomain: "saleplace-cbcc0612.firebaseapp.com",
-    databaseURL: "https://saleplace-cbcc0612.firebaseio.com",
-    projectId: "saleplace-cbcc0612",
-    storageBucket: "saleplace-cbcc0612.appspot.com",
-    messagingSenderId: "1008043798181",
-    appId: "1:1008043798181:web:7cecb342cd5d20fd"
+    apiKey: "<API KEY>",
+    authDomain: "<DOMAIN>",
+    databaseURL: "<DB URL>",
+    projectId: "<PROJECT ID>",
+    storageBucket: "<STORAGE BUCKET>",
+    messagingSenderId: "<MSG SENDER ID>",
+    appId: "<APP ID>"
 };
 
 export default config;
